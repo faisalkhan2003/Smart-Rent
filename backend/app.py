63a3cc81c012f10c70b2dd2337189a5a871904c1
faisalkhan2003 <khanfaisal3774@gmail.com>
2025-07-02 10:20:33 +0530

    Made changes in backend

--- backend/app.py
+++ backend/app.py
@@ -3,18 +3,20 @@ from flask_cors import CORS
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 @app.route("/gemini-api", methods=["POST"])
-def gemini_predict():
-    data = request.json
-    prompt = data["prompt"]
+def gemini_api():
+    data = request.get_json()
+    prompt = data.get("prompt", "")
     try:
-        model = genai.GenerativeModel("models/gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-2.5-flash")
         response = model.generate_content(prompt)
         return jsonify({"prediction": response.text})
     except Exception as e:
